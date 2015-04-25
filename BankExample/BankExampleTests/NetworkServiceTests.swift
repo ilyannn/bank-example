568114289f6f07e1972b91a9f7e4caeaca15d003
ilya <ilya.nikokoshev@gmail.com>
2015-04-26 00:37:30 +0300

    yay test real date in #8

--- BankExample/BankExampleTests/NetworkServiceTests.swift
+++ BankExample/BankExampleTests/NetworkServiceTests.swift
@@ -16,9 +16,22 @@ class NetworkServiceTests: XCTestCase {
         XCTAssert(abs(service1.getServerDate() - service2.getServerDate()) < 1000)
     }
     
-    func testRealServerURL() {
+    func testGenerateServiceURL() {
         let service = RealNetworkService(server: "example.com")
         XCTAssertNotNil(service)
+    }
+    
+    let realServer = "https://raw.githubusercontent.com/ilyannn/bank-example/master/TestService"
+    
+    func testRealServiceDate() {
+        if let service = RealNetworkService(server: realServer) {
+            let time = service.getServerDate()
+            XCTAssertEqual(time, 1429997160)
+            let date = NSDate(timeIntervalSince1970: NSTimeInterval(time))
+            let calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)!
+            let components = calendar.components(.CalendarUnitYear, fromDate: date)
+            XCTAssertEqual(components.year, 2015)
+        }
     }
     
 }
