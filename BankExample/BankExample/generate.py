c05860d9b5607e6506c759e4da572f7c551ebb38
ilya <ilya.nikokoshev@gmail.com>
2015-04-26 20:04:22 +0300

    working on generate for #9

--- BankExample/BankExample/generate.py
+++ BankExample/BankExample/generate.py
@@ -1,24 +1,113 @@
 import sys
+import re
+from string import Template
 
 def process(obj): 
-    print "Processing ", obj
+    print "Processing", obj
     schemap = obj + ".schema"
     swiftp = obj + ".swift"
+
     with open(schemap, 'r') as schemaf:
-        schema = schemaf.read()
+        description = schemaf.read()
+    
+    schema = load_schema(description)
+    print schema
         
     with open(swiftp, 'r') as swiftf:
-        swift = remove_generated(swiftf.read())
+        swift = swiftf.read()
+    
+    generated = generate(obj, schema)
 
     with open(swiftp, 'w') as swiftf:
-        swiftf.write(add_generated(schema, swift))
-        print "File updated at ", swiftp
+        swiftf.write(replace_generated(generated, swift))
+        print "File updated: ", swiftp
 
-def remove_generated(swift):
-    return swift
 
-def add_generated(schema, swift):
-    return swift
+TEMPLATE = Template("""
+    class ${obj}_Generated: Generated { 
+    
+    // Object schema
+    override var keys_generated: [String] {
+        return [${keys}]        
+    }
+    
+    
+    // Generated properties
+    ${generated}
+    
+    
+    // Validation (Objective-C wrappers)
+    ${validate_objc}
+
+    
+    // Validation (native Swift)
+    ${validate_swift}
+}
+""")
+
+BEGIN_GENERATE = "// BEGIN GENERATED CODE: DO NOT EDIT MANUALLY" 
+END_GENERATE = "// END GENERATED CODE"
+
+
+def property_generate(args):
+    key, type = args
+    initial = '""' if type == "string" else '0'
+    return """
+    private(set) var {} = {}
+""".format(key, initial)
+
+
+def validate_objc_generate(args):
+    key, type = args
+    nstype = "NSString" if type == "string" else "NSNumber"
+    
+    return Template("""
+    func validate${Key}(value: ObjectPointer, error: NSErrorPointer) -> Bool {
+        return validate(value) { ($key: ${NSType}) in
+            validate($key: $key.integerValue)
+        }
+    }
+""").substitute(key = key, Key = key.upper(), NSType = nstype)
+
+
+def validate_swift_generate(args):
+    key, type = args
+    initial = '""' if type == "string" else '0'
+    return Template("""
+    func validate(#${key}: ${Type}) -> ValidationError? {
+        return nil
+    }
+""").substitute(key = key, Type = type.upper())
+
+
+def generate(obj, schema):
+    keys = map(lambda x: '"{}"'.format(x), schema.keys())
+    generated = map(property_generate, schema.items())
+    validate_objc = map(validate_objc_generate, schema.items())
+    validate_swift = map(validate_swift_generate, schema.items())
+    
+    return TEMPLATE.substitute(
+        obj = obj, 
+        keys = ', '.join(keys),
+        generated = ''.join(generated), 
+        validate_objc = ''.join(validate_objc),
+        validate_swift = ''.join(validate_swift)
+    )
+
+
+def load_schema(description):
+    schema = {}
+    for line in description.splitlines():
+        matches = re.findall('\w+', line)
+        if len(matches) == 2:
+            schema[matches[0]] = matches[1]
+    return schema
+
+
+def replace_generated(generated, swift):
+    pattern = "(?<={}).*(?={})".format(BEGIN_GENERATE, END_GENERATE)    
+    return re.sub(pattern, generated, swift)
+
 
 for obj in sys.argv[1:]:
     process(obj)
